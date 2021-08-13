63775ec2a4465b5199658588115f842d53b8f808
Sunrise Gong <rhdgoehe2@gmail.com>
2021-08-13 19:34:31 +0900

    multer-s3 and aws-sdk setting

--- prepare/back/routes/post.js
+++ prepare/back/routes/post.js
@@ -4,6 +4,8 @@ const path = require('path');
 const { Post, Comment, Image, User, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 const fs = require('fs');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
 
 const router = express.Router();
 
@@ -14,17 +16,36 @@ try {
     fs.mkdirSync('uploads');
 }
 
+AWS.config.update({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    region: 'us-east-1',
+})
+
 const upload = multer({
-    storage: multer.diskStorage({
-        destination(req, file, done) { done(null, 'uploads') },
-        filename(req, file, done) {
-            const ext = path.extname(file.originalname);
-            const basename = path.basename(file.originalname, ext); 
-            done(null, basename + '_' + new Date().getTime() + ext); 
+    storage: multerS3({ 
+        s3: new AWS.S3(),
+        
+        bucket: 'nodebird-copy-s3',
+        
+        key(req, file, cb) {
+            cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`)// 버킷도 폴더를 생성할 수 있습니다.
         }
     }),
+    
     limits: { fileSize: 20 * 1024 *1024 },
 })
+// const upload = multer({
+//     storage: multer.diskStorage({
+//         destination(req, file, done) { done(null, 'uploads') },
+//         filename(req, file, done) {
+//             const ext = path.extname(file.originalname);
+//             const basename = path.basename(file.originalname, ext); 
+//             done(null, basename + '_' + new Date().getTime() + ext); 
+//         }
+//     }),
+//     limits: { fileSize: 20 * 1024 *1024 },
+// })
 
 // 게시글 등록
 router.post('/', isLoggedIn, upload.none(), async (req, res, next) => { // POST /post
@@ -157,7 +178,8 @@ router.delete('/:postId', isLoggedIn, async (req, res, next) => { // DELETE /pos
 // 이미지 업로드
 router.post('/images', isLoggedIn, upload.array('image'), async (req, res, next) => { // POST /post/images
     console.log(req.files);
-    res.json(req.files.map((v) => v.filename));
+    //res.json(req.files.map((v) => v.filename)); // diskStorage
+    res.json(req.files.map((v) => v.location)); // S3
 });
 
 // 리트윗
