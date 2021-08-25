7091d22df760ed522398643364ccff97be96bff6
Sunrise Gong <rhdgoehe2@gmail.com>
2021-08-25 17:26:21 +0900

    Applying heic-converter

--- prepare/lambda/index.js
+++ prepare/lambda/index.js
@@ -5,21 +5,24 @@ const convert = require('heic-convert');
 const s3 = new AWS.S3();
 
 exports.handler = async (event, context, callback) => { // callback은 passport 의 done 과 비슷합니다.
-    
+
     const Bucket = event.Records[0].s3.bucket.name;
     const Key = decodeURIComponent(event.Records[0].s3.object.key);
     const filename = Key.split('/')[Key.split('/').length - 1];
     const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase();
-    const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; 
-    
+    const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
+
     console.log('버킷', Bucket, '키', Key, '파일명', filename, '확장자', requiredFormat);
-    
+
     try {
         const s3Object = await s3.getObject({ Bucket, Key }).promise();
-        
+
         console.log('원본 이미지 용량', s3Object.Body.length);
 
-        const resizedImage = await sharp(s3Object.Body)
+        const s3ObjectConvert = ext === 'heic' ?
+            await convert({ buffer: s3Object, format: 'JPEG', quality: 1 }) : await s3Object;
+
+        const resizedImage = await sharp(s3ObjectConvert.Body)
             .resize(400, 400, { fit: 'inside' }) // 사이즈 변경
             .toFormat(requiredFormat) // 확장자 지정
             .toBuffer();
@@ -29,7 +32,7 @@ exports.handler = async (event, context, callback) => { // callback은 passport 
             Key: `thumb/${filename}`,
             Body: resizedImage // 리사이즈 된 이미지 데이터
         }).promise();
-        
+
         console.log('리사이즈된 이미지', resizedImage.length);
 
         return callback(null, `thumb/${filename}`);
@@ -55,7 +58,7 @@ filename(파일명 추출)
 ex) original/65436_abc.png -> 65436_abc.png
 
 ext(확장자명 추출)
-ex) original/65436_abc.png -> png , 
+ex) original/65436_abc.png -> png ,
 toLowerCase : 대문자로 들어가는 것 방지하기 위해
 
 requiredFormat(최종 확장자명)
