0480f47435474f6c8002494b9e5d96c0f0434a79
Sunrise Gong <rhdgoehe2@gmail.com>
2021-09-06 17:44:32 +0900

    Resolving the problem that the image is broken for a while after uploading the image

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,45 +1,103 @@
 // PostCard.js 의 자식
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { PlusOutlined } from '@ant-design/icons';
 
 import ImagesZoom from './ImagesZoom'; // 폴더만 써도 자동으로 그 안의 index.js가 임폴트
 
+function isImageValid(src) {
+    return new Promise((resolve) => {
+        const img = document.createElement('img');
+        img.onerror = () => resolve(false);
+        img.onload = () => resolve(true);
+        img.src = src;
+    });
+}
+
 const PostImages = ({ images }) => {
     const [showImagesZoom, setShowImagesZoom] = useState(false);
-    
+
+    const imgEl = useRef();
+
+    useEffect(() => {
+        isImageValid(images[0].src)
+            .then((isValid) => {
+                if (!isValid) { imgEl.current.remove(); }
+            });
+    }, [images[0].src]);
+
     const onZoom = useCallback(() => { setShowImagesZoom(true); }, []);
 
     const onClose = useCallback(() => { setShowImagesZoom(false); }, []);
-    
+
+    const onError = useCallback((e) => {
+        console.dir(e.target);
+        e.target.previousSibling.remove();
+        // e.target.src = e.target.currentSrc.replace('/thumb/', '/original/');
+    }, []);
+
     if (images.length === 1) {
         return (
             <>
-                <img role="presentation" src={`${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+                <picture>
+                    <source ref={imgEl} srcSet={`${images[0].src}`} />
+                    <img
+                        role="presentation"
+                        onClick={onZoom}
+                        src={`${images[0].src.replace('/thumb/', '/original/')}`}
+                        alt={images[0].src}
+                        style={{ maxWidth: '100%', display: 'inline-block' }}
+                    />
+                </picture>
                 {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
             </>
         );
     }
-    
+
     if (images.length === 2) {
         return (
             <>
-                <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`${images[0].src}`} alt={images[0].src} onClick={onZoom} />
-                <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`${images[1].src}`} alt={images[1].src} onClick={onZoom} />
+                <picture style={{ width: '50%', display: 'inline-block' }}>
+                    <source ref={imgEl} srcSet={`${images[0].src}`} />
+                    <img
+                        role="presentation"
+                        onClick={onZoom}
+                        src={`${images[0].src.replace('/thumb/', '/original/')}`}
+                        alt={images[0].src} />
+                    style={{ maxWidth: '100%', display: 'inline-block' }}
+                </picture>
+                <picture style={{ width: '50%', display: 'inline-block' }}>
+                    <source ref={imgEl} srcSet={`${images[1].src}`} />
+                    <img
+                        role="presentation"
+                        onClick={onZoom}
+                        src={`${images[1].src.replace('/thumb/', '/original/')}`}
+                        alt={images[1].src} />
+                    style={{ maxWidth: '100%', display: 'inline-block' }}
+                </picture>
                 {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
             </>
         );
     }
-    
+
     return (
         <>
             <div>
-                <img role="presentation" style={{ width: '50%' }} src={`${images[0].src}`} alt={images[0].src} onClick={onZoom} />
-                
+                <picture style={{ width: '50%' }} onError={onError}>
+                    <source ref={imgEl} srcSet={`${images[0].src}`} />
+                    <img
+                        role="presentation"
+                        onClick={onZoom}
+                        src={`${images[0].src.replace('/thumb/', '/original/')}`}
+                        alt={images[0].src}
+                        style={{ maxWidth: '50%' }}
+                    />
+                </picture>
                 <div
-                    role="presentation" 
+                    role="presentation"
                     style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
-                    onClick={onZoom}>
+                    onClick={onZoom}
+                >
                     <PlusOutlined />
                     <br />
                     {images.length - 1}개의 사진 더보기
